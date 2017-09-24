869005a47a09ca193f8fb0185451be31096bcb6d
Erlend Stuyven <erlend.stuyven@cegeka.com>
2017-09-24 12:40:39 +0200

    Initial commit

---
+++ src/test/java/BowlingGameTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+public class BowlingGameTest {
+
+    @Test
+    public void roll() {
+        throw new RuntimeException("not yet implemented");
+    }
+
+    @Test
+    public void score() {
+        throw new RuntimeException("not yet implemented");
+    }
+
+
+}
